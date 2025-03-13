9f1275e612641b49499519ea916564841ab77642
Vinicius Nunes <viniiciuspereiira@hotmail.com>
2025-03-12 23:44:49 -0300

    feat: reestruturação do projeto e conexão com o redis

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,20 +1,55 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"context"
+	"fila-flex/internal/server"
+	"fmt"
+	"log"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
-	// Criar um novo roteador Gin
-	r := gin.Default()
+	server := server.NewServer()
 
-	// Definir uma rota GET para /hello
-	r.GET("/hello", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello, World!",
-		})
-	})
+	// Create a done channel to signal when the shutdown is complete
+	done := make(chan bool, 1)
 
-	// Iniciar o servidor na porta 8080
-	r.Run(":8080")
+	// Run graceful shutdown in a separate goroutine
+	go gracefulShutdown(server, done)
+
+	err := server.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		panic(fmt.Sprintf("http server error: %s", err))
+	}
+
+	// Wait for the graceful shutdown to complete
+	<-done
+	log.Println("Graceful shutdown complete.")
+}
+
+func gracefulShutdown(apiServer *http.Server, done chan bool) {
+	// Create context that listens for the interrupt signal from the OS.
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	// Listen for the interrupt signal.
+	<-ctx.Done()
+
+	log.Println("shutting down gracefully, press Ctrl+C again to force")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := apiServer.Shutdown(ctx); err != nil {
+		log.Printf("Server forced to shutdown with error: %v", err)
+	}
+
+	log.Println("Server exiting")
+
+	// Notify the main goroutine that the shutdown is complete
+	done <- true
 }
---
+++ internal/server/server.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"fila-flex/internal/database"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+type Server struct {
+	port int
+	db   database.Service
+}
+
+func NewServer() *http.Server {
+	port, _ := strconv.Atoi(os.Getenv("PORT"))
+	NewServer := &Server{
+		port: port,
+		db:   database.New(),
+	}
+
+	// Declare Server config
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", NewServer.port),
+		Handler:      NewServer.RegisterRoutes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	return server
+}
